d93bbe299829eadd49786d20dc5331fdc6a6c37c
unknown <mariem.messaoudi@esprit.tn>
2022-04-07 02:38:16 +0200

    EntrepriseServiceImplTest1

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -1,26 +1,25 @@
 package tn.esprit.spring;
 
 import static org.junit.Assert.*;
-
-import java.util.List;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.List;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.EntrepriseRepository;
+import tn.esprit.spring.services.IEntrepriseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import tn.esprit.spring.entities.Entreprise;
-import tn.esprit.spring.repository.EntrepriseRepository;
-import tn.esprit.spring.services.IEntrepriseService;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class EntrepriseServiceImplTest {
 	
 	private static final Logger l = LogManager.getLogger(EntrepriseServiceImplTest.class);
-
+	
 	@Autowired
 	IEntrepriseService ientrepriseservice;
 	
@@ -29,26 +28,33 @@ public class EntrepriseServiceImplTest {
 	
 	Integer idE;
 	
-	
 	@Test
-	public void testAjouterEntreprise()  { 
-		 l.debug("methode ajouterEntreprise");
+	public void testAjouterEntreprise() {
+		l.debug("Test méthode AjouterEntreprise");
 		 try {
-		 idE=ientrepriseservice.ajouterEntreprise(new Entreprise("test_nom_entreprise","test_raison_sociale "));
-		assertNotNull(idE);
+			 idE = ientrepriseservice.ajouterEntreprise(new Entreprise("vermeg","raisonSociale "));
+			 assertNotNull(idE);
 		 } catch (Exception e) {
-		       l.error("erreur methode ajouterEntreprise :" +e);	
+		       l.error("méthode AjouterEntreprise error :" + e);	
 
 			}
 	}
-	
 
 	@Test
 	public void testGetAllDepartementsNamesByEntreprise() {
-		 l.debug("methode GetAllDepartementNAmesByEntreprise");
+		l.debug("Test méthode GetAllDepartementsNamesByEntreprise");
 		List<String> depNames = ientrepriseservice.getAllDepartementsNamesByEntreprise(1);
 		assertNotNull(depNames);
-	}	
+	}
+
+	@Test
+	public void testDeleteEntrepriseById() {
 	
-	
+	}
+
+	@Test
+	public void testGetEntrepriseById() {
+		
+	}
+
 }
